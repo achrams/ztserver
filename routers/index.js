56c927c831f3819fee30a83d2d11e76dac6a4cd7
Achmad Ramadhan Tamherwarin <achmad.ramadhan-consultant@prudential.co.id>
2023-05-09 15:11:22 +0700

    add cors

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router()
 const user = require('./users')
 const applicants = require('./applicants')
-
 router.get('/', (req, res) => {
   res.send('Connected!')
 })
-
 router.use('/users', user)
 router.use('/applicants', applicants)
 
